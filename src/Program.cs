e39d139fbf1fde2dbec8a25adccd82fa3a6ddfad
aziirehman <AUr-rehman2@uclan.ac.uk>
2023-11-01 12:51:55 +0000

    added while loop and if statement

--- src/Program.cs
+++ src/Program.cs
@@ -8,19 +8,28 @@
         int helmetPrice = 3000;
         int foodPrice = 500;
         int presentPrice = 1000;
+        int jacketPrice=3200;
 
         Console.WriteLine("------------------------------");
         Console.WriteLine("Welcome to the general store!");
         Console.WriteLine("------------------------------");
 
         Console.WriteLine("");
-
+     
+    
+       
+   
+   
+while(playerCoins>0)
+{
         Console.WriteLine("What would you like to purchase?");
+         Console.WriteLine("");
 
         Console.WriteLine("A: A nice shirt.");
         Console.WriteLine("B: A protective helmet.");
         Console.WriteLine("C: Some food.");
         Console.WriteLine("D: A present.");
+        Console.WriteLine("E: A Jacket.");
 
         Console.WriteLine("");
 
@@ -29,24 +38,68 @@
 
         Console.WriteLine("");
 
-        switch (purchaseLetter) {
+ 
+ 
+        switch (purchaseLetter) 
+        {
             case "A":
+                 
                 playerCoins = playerCoins - shirtPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                
+                
+           if(playerCoins>0)
+        {
+          Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                       
+                }
+                else {Console.WriteLine($"Thanks for your purchase! you have no Coins remaining.") ;}
+             
                 break;
+                
+            
+           
             case "B":
+              
                 playerCoins = playerCoins - helmetPrice;
+                if(playerCoins>0){
                 Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                }
+                else {Console.WriteLine($"Thanks for your purchase! you have no Coins remaining.") ;}
                 break;
+
             case "C":
                 playerCoins = playerCoins - foodPrice;
+
+                 if(playerCoins>0){
                 Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                }
+                else {Console.WriteLine($"Thanks for your purchase! you have no Coins remaining.") ;}
                 break;
             case "D":
                 playerCoins = playerCoins - presentPrice;
+                 if(playerCoins>0){
                 Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                }
+                else {Console.WriteLine($"Thanks for your purchase! you have no Coins remaining.") ;}
                 break;
-        }
+            case "E":
+               playerCoins = playerCoins -jacketPrice;
+               if(playerCoins>0){
+                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                }
+                else {Console.WriteLine($"Thanks for your purchase! you have no Coins remaining.") ;}
+                break; 
+            default:
+            Console.WriteLine("letter doesn't recognize.Enter again");
+            break;
+         
+            
+        
+ }
+}  
+    
+    
+    
     }
 }
 
